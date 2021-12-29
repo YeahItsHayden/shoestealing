f71a7a8045e30ac299a44f25d2bab54906563e04
YeahItsHayden <haydenparsons2@gmail.com>
2021-12-29 13:00:26 +1000

    initial commit

---
+++ cl_main.lua
@@ -0,0 +1,33 @@
+function stealShoes(ent)
+    stealShoesEmote()
+    SetPedComponentVariation(ent, 6, 34, 0, 0)
+end
+
+function getPedPlayerIsLookingAt()
+	local plyPos = GetEntityCoords(PlayerPedId())
+	local plyOffset = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.3, 0.0)
+	local rH = StartShapeTestCapsule(plyPos.x, plyPos.y, plyPos.z, plyOffset.x, plyOffset.y, plyOffset.z, 1.0, 12, PlayerPedId(), 7)
+	local _, _, _, _, ent = GetShapeTestResult(rH)
+	return ent
+end
+
+function stealShoesEmote()
+	local Ped = PlayerPedId()
+    local dict = "random@domestic"
+
+    RequestAnimDict(dict)
+    while (not HasAnimDictLoaded(dict)) do Wait(0) end
+	TaskPlayAnim(Ped, dict, "pickup_low", 3.0, 3.0, 1200, 0, 0, false, false, false)
+	local Pause = 1200-500 if Pause < 500 then Pause = 500 end
+end
+
+RegisterCommand("stealshoes", function()
+    local player = getPedPlayerIsLookingAt()
+    local ply = PlayerPedId()
+
+    if IsPedAPlayer(player) then
+        if not config.shoes == GetPedDrawableVariation(PlayerPedId(), 6) then 
+            stealShoes(player)
+        end
+    end
+end, false)
